a1598089952017e06fe018ee71574ae8e9bc06a1
Alex Chistyakov <alexclear@gmail.com>
2015-03-02 16:14:15 +0300

    Clean more files

--- image/cleanup.sh
+++ image/cleanup.sh
@@ -3,7 +3,7 @@ set -e
 source /build/buildconfig
 set -x
 
-apt-get -y remove g++ g++-4.8 gcc gcc-4.8 libtool
+apt-get -y remove g++ g++-4.8 gcc gcc-4.8 libtool patch gawk make libc6-dev libreadline6-dev zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 autoconf libgdbm-dev libncurses5-dev automake bison pkg-config libffi-dev
 apt-get -y autoremove
 apt-get clean
 rm -rf /build
