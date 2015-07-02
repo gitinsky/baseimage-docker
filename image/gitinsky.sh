cff0515dff990e7eb18946d6a5ec4a39827027b6
Alex Chistyakov <alexclear@gmail.com>
2015-07-02 15:33:31 +0300

    Put commands in proper order

--- image/gitinsky.sh
+++ image/gitinsky.sh
@@ -2,8 +2,8 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
-rvm install ruby-2.1
 apt-get -y install git libyaml-dev
+rvm install ruby-2.1
 cd /root && git clone https://github.com/gitinsky/eye.git
 cd /root/eye && gem build eye.gemspec && bash -c "gem install eye-*.gem"
 cp /build/rc.local /etc/rc.local
