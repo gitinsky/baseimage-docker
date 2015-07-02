7f0270cf47b3bea2a68940491d4c0325ad645eb9
Alex Chistyakov <alexclear@gmail.com>
2015-07-02 14:51:42 +0300

    Fix eye

--- image/gitinsky.sh
+++ image/gitinsky.sh
@@ -3,7 +3,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm install ruby-2.1
-apt-get -y install git
+apt-get -y install git libyaml-dev
 cd /root && git clone https://github.com/gitinsky/eye.git
 cd /root/eye && gem build eye.gemspec && bash -c "gem install eye-*.gem"
 cp /build/rc.local /etc/rc.local
