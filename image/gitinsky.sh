86b47ffe17ca283ed02ea46b8562e32cc41e91a0
Alex Chistyakov <alexclear@gmail.com>
2015-07-02 16:17:33 +0300

    Install libyaml-0-2 explicitly

--- image/gitinsky.sh
+++ image/gitinsky.sh
@@ -2,7 +2,7 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
-apt-get -y install git libyaml-dev
+apt-get -y install git libyaml-0-2 libyaml-dev
 rvm install ruby-2.1
 cd /root && git clone https://github.com/gitinsky/eye.git
 cd /root/eye && gem build eye.gemspec && bash -c "gem install eye-*.gem"
